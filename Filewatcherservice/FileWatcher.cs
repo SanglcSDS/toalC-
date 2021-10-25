7af1b893f7ded31b4d4b30041d7c496f1145f0ad
sanglcsds <sanglc@mitec.com.vn>
2021-10-25 17:44:14 +0700

    sanglc

---
+++ Filewatcherservice/FileWatcher.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+namespace Filewatcherservice
+{
+    public class FileWatcher {
+
+        private FileSystemWatcher _filewatcher;
+        public FileWatcher() {
+            _filewatcher = new FileSystemWatcher(PathLocation());
+            _filewatcher.Changed += new FileSystemEventHandler(_filewatcher_Changed);
+            _filewatcher.Created += new FileSystemEventHandler(_filewatcher_Created);
+          //  _filewatcher.Renamed += new FileSystemEventHandler(_filewatcher_Renamed);
+            _filewatcher.Deleted += new FileSystemEventHandler(_filewatcher_Deleted);
+            _filewatcher.EnableRaisingEvents = true;
+            _filewatcher.IncludeSubdirectories = true;
+        }
+        private string PathLocation()
+        {
+            string value = string.Empty;
+            try {
+                value = ConfigurationSettings.AppSettings["location"];
+
+            } catch(Exception ex)
+            {
+
+            }
+            return value;
+
+
+
+        }
+        void _filewatcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            Logger.Log(string.Format("File:{0} Changed at time:{1}", e.Name, DateTime.Now.ToLocalTime()));
+
+        }
+        void _filewatcher_Created(object sender, FileSystemEventArgs e)
+        {
+            Logger.Log(string.Format("File:{0} Created at time:{1}", e.Name, DateTime.Now.ToLocalTime()));
+
+        }
+        void _filewatcher_Renamed(object sender, FileSystemEventArgs e)
+        {
+            Logger.Log(string.Format("File:{0} Renamed at time:{1}", e.Name, DateTime.Now.ToLocalTime()));
+
+        }
+        void _filewatcher_Deleted(object sender, FileSystemEventArgs e)
+        {
+            Logger.Log(string.Format("File:{0} Deleted at time:{1}", e.Name, DateTime.Now.ToLocalTime()));
+
+        }
+
+    }
+}
---
+++ Filewatcherservice/Logger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Filewatcherservice
+{
+   public static class Logger
+       
+    {
+        public static void Log(string message)
+        {
+            try
+            {
+                string _message = string.Format("{0}{1}",message,Environment.NewLine);
+                File.AppendAllText(AppDomain.CurrentDomain.BaseDirectory+ "logFile.txt",_message);
+
+            }catch(Exception ex)
+            {
+
+            }
+        }
+    
+    }
+}
